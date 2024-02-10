2556fdbd7270c8804176e4bddcfdf3b4ab29da73
Harini-032003 <hariniv200303@gmail.com>
2024-02-11 00:45:20 +0530

    data fetched from database

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config(); // to use environment variables from .env file
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import productRoutes from './routes/productRoutes.js';
 const port=process.env.PORT || 5000; // if env variable PORT is not found, then use port 5000.
 // to access env variables, use the prefix process.env.(env_variable_name)
 connectDB();
@@ -10,13 +11,7 @@ const app=express();
 app.get('/',(req, res) => {   // setting first route
   res.send('API is running...');
 });   
-
-app.get('/api/products', (req, res)=>{
-  res.json(products); //response is a json file
-});
-
-app.get('/api/products/:id', (req, res)=>{
-  const product= products.find((p)=> p._id === req.params.id);
-  res.json(product);
-});
+ app.use('/api/products',productRoutes); //whenever we hit this api/products endpoint it will check the file productRoutes where we have separately mentioned and check the corresponding end point specified there and send response
+app.use(notFound);
+app.use(errorHandler);
 app.listen(port, () => console.log(`Server running on port ${port}`));
