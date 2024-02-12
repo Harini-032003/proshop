34322bc07ecd9ef9824876c95fa952a268f3d3b3
Harini-032003 <hariniv200303@gmail.com>
2024-02-12 22:16:01 +0530

    checkout handler added

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -19,6 +19,10 @@ const CartScreen = () => {
     dispatch(removeFromCart(id));
   };
 
+  const checkoutHandler = () => {
+    navigate('/login?redirect=/shipping');
+  };
+
   return <Row>
     <Col md = {8}>
       <h1 style = {{marginBottom: '20px'}}>Shopping Cart</h1>
@@ -67,7 +71,7 @@ const CartScreen = () => {
           ${ cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)}
         </ListGroup.Item>
         <ListGroup.Item>
-           <Button type='button' className='btn-block' disabled = {cartItems.length === 0}>
+           <Button type='button' className='btn-block' disabled = {cartItems.length === 0} onClick={checkoutHandler}>
              Proceed to Checkout
            </Button>
         </ListGroup.Item>
