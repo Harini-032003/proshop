2c567c4e3cf1e724dd45ed6945412d624894e4e3
Harini-032003 <hariniv200303@gmail.com>
2024-02-22 21:17:31 +0530

    order route and paypal route configured

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 const port=process.env.PORT || 5000; // if env variable PORT is not found, then use port 5000.
 // to access env variables, use the prefix process.env.(env_variable_name)
 connectDB();
@@ -22,6 +23,11 @@ app.get('/',(req, res) => {   // setting first route
 });   
  app.use('/api/products',productRoutes); //whenever we hit this api/products endpoint it will check the file productRoutes where we have separately mentioned and check the corresponding end point specified there and send response
  app.use('/api/users', userRoutes);
+ app.use('/api/orders', orderRoutes);
+
+app.get('/api/config/paypal', (req,res) =>  res.send({ clientId: process.env.PAYPAL_CLIENT_ID})
+); 
+
 app.use(notFound);
 app.use(errorHandler);
 app.listen(port, () => console.log(`Server running on port ${port}`));
