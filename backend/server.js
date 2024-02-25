0a3ad6edd401a2f756a73c4b6162fdcb9edd188d
Harini-032003 <hariniv200303@gmail.com>
2024-02-25 21:47:41 +0530

    upload routes added

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -7,6 +8,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js';
 const port=process.env.PORT || 5000; // if env variable PORT is not found, then use port 5000.
 // to access env variables, use the prefix process.env.(env_variable_name)
 connectDB();
@@ -24,9 +26,14 @@ app.get('/',(req, res) => {   // setting first route
  app.use('/api/products',productRoutes); //whenever we hit this api/products endpoint it will check the file productRoutes where we have separately mentioned and check the corresponding end point specified there and send response
  app.use('/api/users', userRoutes);
  app.use('/api/orders', orderRoutes);
-
+ app.use('/api/upload', uploadRoutes);
 app.get('/api/config/paypal', (req,res) =>  res.send({ clientId: process.env.PAYPAL_CLIENT_ID})
 ); 
+
+const __dirname = path.resolve();//set __dirname to current directory
+app.use('/uploads', express.static(path.join(__dirname, '/uploads'))); 
+
+
 
 app.use(notFound);
 app.use(errorHandler);
