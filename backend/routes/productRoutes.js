4cb53b6f825d7b6d392e73c0bc2f33674f8ce694
Harini-032003 <hariniv200303@gmail.com>
2024-02-25 21:36:05 +0530

    routes for creating,updating and deleting product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,9 +3,16 @@ const router = express.Router();
 import {
   getProducts,
   getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  createProductReview
 } from '../controllers/productController.js';
-//using functions from productController to getProducts and product by id
-router.route('/').get(getProducts);
-router.route('/:id').get(getProductById);
+// middleware for admin routes
+import {protect, admin} from '../middleware/authMiddleware.js';
 
+//using functions from productController to getProducts and product by id
+router.route('/').get(getProducts).post(protect,admin, createProduct);
+router.route('/:id').get(getProductById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);
+router.route('/:id/reviews').post(protect, createProductReview);
 export default router;
