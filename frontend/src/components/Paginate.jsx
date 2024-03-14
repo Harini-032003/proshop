91add40f8fa784bb09828aa1583ab8b434f01499
Harini-032003 <hariniv200303@gmail.com>
2024-03-14 22:57:45 +0530

    pagination for search updated

--- frontend/src/components/Paginate.jsx
+++ frontend/src/components/Paginate.jsx
@@ -1,14 +1,14 @@
 import { Pagination } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
-const Paginate = ({ pages, page, isAdmin = false }) => { //isAdmin cuz, use this in page list feature for admin
+const Paginate = ({ pages, page, isAdmin = false,keyword = '' }) => { //isAdmin cuz, use this in page list feature for admin
   return (
     pages > 1 && (
       <Pagination>
         {[...Array(pages).keys()].map((x) => (
           <LinkContainer
             key={x + 1} //{/*keys indicate page number. they start from 0, so x+1  */}
-            to={!isAdmin ? `/page/${x + 1}` : `/admin/productlist/${x + 1}`}
+            to={!isAdmin ? keyword ? `/search/${keyword}/page/${x+1}`:`/page/${x + 1}` : `/admin/productlist/${x + 1}`}
           >
             <Pagination.Item active={x + 1 === page}>{x + 1}</Pagination.Item>
           </LinkContainer>
