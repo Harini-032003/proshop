8ba48e4decc6c4ec04d9c4196202b3f23ad877e1
Harini-032003 <hariniv200303@gmail.com>
2024-04-24 20:49:12 +0530

    server updated

--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,7 @@ app.use(express.urlencoded({ extended: true}));
 
 //cookie parser middleware
 app.use(cookieParser());
-app.get('/',(req, res) => {   // setting first route
-  res.send('API is running...');
-});   
+   
  app.use('/api/products',productRoutes); //whenever we hit this api/products endpoint it will check the file productRoutes where we have separately mentioned and check the corresponding end point specified there and send response
  app.use('/api/users', userRoutes);
  app.use('/api/orders', orderRoutes);
@@ -33,6 +31,8 @@ app.get('/api/config/paypal', (req,res) =>  res.send({ clientId: process.env.PAY
 const __dirname = path.resolve();//set __dirname to current directory
 app.use('/uploads', express.static(path.join(__dirname, '/uploads'))); 
 
+
+
 if (process.env.NODE_ENV === 'production') {
   //set static folder
   app.use(express.static(path.join(__dirname, '/frontend/build')));
@@ -40,7 +40,8 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   );// if the user wants to access any endpoint that is not available redirect to home page
-} else {
+} 
+ else {
 
   app.get('/', (req, res) => {
     res.send('API is running....');
